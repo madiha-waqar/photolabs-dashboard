d0771f40a9f4fcf0f7fe8844c45b816479e86d9e
madiha-waqar <mail2madihawaqar@gmail.com>
2023-09-08 13:14:58 -0400

    Add mock data to render photolab details for panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,35 @@ import React, { Component } from "react";
 import Loading from "./Loading";
 
 import classnames from "classnames";
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza"
+  }
+];
+
 
 class Dashboard extends Component {
   state = {
     loading: false // Change the initial value to false
   };
+ 
 
   render() {
     const dashboardClasses = classnames("dashboard");
